9756b456021065f59a7154ff4caf5f6c2af18e93
anurag1109 <bittu.anurag123@gmail.com>
2023-06-15 22:57:16 +0530

    API Swagger added for Documnetation of Routes

--- backend/index.js
+++ backend/index.js
@@ -1,26 +1,63 @@
 const express = require("express");
-const {connection} = require("./config/db")
-const {bookingRoute} = require("./controller/booking.route")
-const cors = require('cors');
+const { connection } = require("./config/db");
+const { bookingRoute } = require("./controller/booking.route");
+const cors = require("cors");
 const { userRoute } = require("./controller/user.route");
 require("dotenv").config();
 
 const app = express();
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 
-app.get("/",(req,res)=>{
-    res.send({"<backendlink>/":"default route (add all routes here to show at home page of backend server )"})
-})
+app.get("/", (req, res) => {
+  res.send({
+    "<backendlink>/":
+      "default route (add all routes here to show at home page of backend server )",
+  });
+});
 
 app.use("/booking", bookingRoute);
-app.use("/user",userRoute)
+app.use("/user", userRoute);
 
-app.listen(process.env.serverPort, async()=>{
-    try {
-        await connection
-        console.log("Server: "+process.env.serverPort);
-    } catch (error) {
-        console.log("Error: "+error.message);
-    }
-})
+//SWAGGER________________________________________________________________
+
+const swaggerJSDoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Node JS API Project for FlexFit",
+      version: "1.0.0",
+      description:
+        "About : - Documentation of application FlexFit which is a Fitness class Booking application in which you can hire Top quality Trainers or become a Trainer itself.",
+      license: {
+        name: "FlexFit",
+      },
+      contact: {
+        name: "FlexFit",
+        url: "flexfit.com",
+        email: "flexfit@gmail.com",
+      },
+    },
+    servers: [
+      {
+        url: `http://localhost:${process.env.serverPort}`,
+      },
+    ],
+  },
+  apis: ["./controller/*.js"],
+};
+const swaggerSpec = swaggerJSDoc(options);
+app.use("/apidocs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+//Listen to server________________________________________________________________
+
+app.listen(process.env.serverPort, async () => {
+  try {
+    await connection;
+    console.log("Server: " + process.env.serverPort);
+  } catch (error) {
+    console.log("Error: " + error.message);
+  }
+});
